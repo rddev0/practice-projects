79933e9d3dc50f85495fc880bd84c738b2d6b579
Rafael Duarte <rd.dev.2022@gmail.com>
2022-09-30 22:32:23 -0300

    update for side practice

---
+++ google-task-clone/side-practice/index.js
@@ -0,0 +1,64 @@
+
+
+
+
+//next S5E1
+
+
+// --- S4.E1 ---
+
+
+/*
+// --- S3.E4 ---
+let age = 31;
+let message = 'I am ' + age + ' years old';
+console.log(message);
+console.log(typeof message);
+*/
+
+/*
+// --- S3.E3 ---
+let operation1 = 4+5*3;
+let operation2 =(4+5)*3;
+console.log(operation1);
+console.log(operation2);
+*/
+
+/*
+// --- S3.E2 ---
+let dayOfMonth = '30';
+let month = 'september';
+let year = '2022';
+
+console.log(month + ' ' + dayOfMonth + ', ' + year);
+*/
+
+/*
+// --- S3.E1 ---
+let todo1;
+todo1 = 99;
+typeOfTodo1 = typeof todo1;
+console.log(typeOfTodo1);
+*/
+
+/*
+// --- S1.E4 ---
+document.body.innerHTML = '';
+*/
+
+/*
+// --- S1.E3 ---
+document.body.innerHTML = 'Rafael';
+
+//script tag must come after page load
+*/
+
+/*
+// --- S1.E2 e S2.E2 ---
+console.log('Friday, September 30, 2022');
+*/
+
+/*
+// --- S1.E1 ---
+alert('Rafael');
+*/
